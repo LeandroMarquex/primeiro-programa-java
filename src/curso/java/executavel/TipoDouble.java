ba01b19b05483972a977ec862241116ea3f5ab4e
Administrator <Administrator@SERVERNOTE>
2023-10-18 14:14:29 -0300

    Criada classes TipoInt e TipoDouble

---
+++ src/curso/java/executavel/TipoDouble.java
@@ -0,0 +1,20 @@
+package curso.java.executavel;
+
+public class TipoDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double nota1 = 90;
+		double nota2 = 90;
+		double nota3 = 70;
+		double nota4 = 90;
+		
+		double mediaFinal = ((nota1 + nota2 + nota3 + nota4) / 4);
+		
+		System.out.println("A média é: " + mediaFinal );
+
+
+	}
+
+}
---
+++ src/curso/java/executavel/TipoInt.java
@@ -0,0 +1,16 @@
+package curso.java.executavel;
+
+public class TipoInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota1 = 90;
+		int nota2 = 90;
+		int nota3 = 70;
+		int nota4 = 90;
+		
+		System.out.println("A média é: " + ((nota1 + nota2 + nota3 + nota4) / 4) );
+
+	}
+
+}
