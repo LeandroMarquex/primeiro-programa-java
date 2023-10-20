96e4c23935d5d1b03876d1176635b93e91a1732d
Administrator <Administrator@SERVERNOTE>
2023-10-19 21:22:47 -0300

    Classe tipo Char e String

---
+++ src/curso/java/executavel/TipoChar.java
@@ -0,0 +1,17 @@
+package curso.java.executavel;
+
+public class TipoChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char pessoaFeminino = 'F';
+		
+		if(pessoaFeminino == 'F') {
+			System.out.println("Feminino");
+		} else {
+			System.out.println("Masculino");
+		}
+
+	}
+
+}
---
+++ src/curso/java/executavel/TipoString.java
@@ -0,0 +1,18 @@
+package curso.java.executavel;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = "Leandro Marques";
+		String cpf = "039.791.379-61";
+		String telefone = "43 99167-4093";
+		String endereco = "Londrina";
+		
+		System.out.println("Meu nome: " + nome 
+				+ " CPF: " + cpf + " Meu telefone: " 
+				+ " Endereço: " + endereco);
+
+	}
+
+}
