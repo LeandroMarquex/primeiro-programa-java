110012fbf954f6cbfbdd506b25a15dc8b642ae0e
Administrator <Administrator@SERVERNOTE>
2023-10-23 22:04:06 -0300

    Operadores Relacionais

---
+++ src/curso/java/executavel/OperadoresRelacionais.java
@@ -0,0 +1,23 @@
+package curso.java.executavel;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota1 = 40;
+		int nota2 = 50;
+		int nota3 = 50;
+		int nota4 = 50;
+		int media = 0;
+
+		// Operadores RELACIONAIS
+		
+
+		if (nota1 >= nota2) {
+			System.out.println("nota 1 é  " + nota1 + " >= " + "  nota 2 é " + nota2);
+		} else {
+			System.out.println("nota 1 é  " + nota1 +" < " + "nota 2 é " + nota2 );
+		}
+	}
+
+}
